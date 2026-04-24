e01eb0b3ca697c65bf4113b3e510c9efb42e048d
vtrsc <vitorsc1998@gmail.com>
2026-04-24 14:51:35 -0300

    Criando e Treinando Minha Primeira Rede Neural Para Determinar a Categoria de Alunos

--- exemplo-00/index.js
+++ exemplo-00/index.js
@@ -1,47 +1,83 @@
-import * as tf from '@tensorflow/tfjs';
-
-async function trainModel( inputXs, outputYs) {
-    const model = tf.sequential();
+import * as tf from '@tensorflow/tfjs'
 
 
-    //primeira camada da rede:
-    // entrada de 7 posições (idade normalizada + 3 cores + 3 localizaçoes)
-    // 80 neurônios = aqui coloquei tudo isso , pq tem pouca base de treino
-    //quanto mais neurônios, mais complexa a rede pode aprender
- // e consequentemente, mais processamento ela vai usar 
+
+async function trainModel(inputXs, outputYs) {
+    const model = tf.sequential()
+
+    // Primeira camada da rede:
+    // entrada de 7 posições (idade normalizada + 3 cores + 3 localizacoes)
+
+    // 80 neuronios = aqui coloquei tu
+    // do isso, pq tem pouca base de treino
+    // quanto mais neuronios, mais complexidade a rede pode aprender
+    // e consequentemente, mais processamento ela vai usar
 
     // A ReLU age como um filtro:
     // É como se ela deixasse somente os dados interessantes seguirem viagem na rede
     /// Se a informação chegou nesse neuronio é positiva, passa para frente!
     // se for zero ou negativa, pode jogar fora, nao vai servir para nada
-    model.add(tf.layers.dense({inputShape:[7], units: 80 , activation: 'relu'}))
+    model.add(tf.layers.dense({ inputShape: [7], units: 80, activation: 'relu' }))
 
-    //saida: 3 neurônios, um para cada categoria (premium, medium, basic)
-    // activation : softmax normaliza a saída para que a soma seja 1, ou seja, cada neurônio vai representar a probabilidade de cada categoria
-    model.add(tf.layers.dense({units: 3, activation: 'softmax'}))
+    // Saída: 3 neuronios
+    // um para cada categoria (premium, medium, basic)
 
-    // compilando o modelo 
-    // optimizer: 'adam' {Adaptive Moment Estimation}
-    // é um treinador pessoal moderno para redes neurais
+    // activation: softmax normaliza a saida em probabilidades
+    model.add(tf.layers.dense({ units: 3, activation: 'softmax' }))
+
+    // Compilando o modelo
+    // optimizer Adam ( Adaptive Moment Estimation)
+    // é um treinador pessoal moderno para redes neurais:
     // ajusta os pesos de forma eficiente e inteligente
     // aprender com historico de erros e acertos
-    // loss : 'categoricalCrossentropy'
-    // ele compara o que o modelo "acha" (os scores de cada categoria)
-    // com o que é a resposta correta
-    // a categoria premium sera sempre [1, 0, 0]
 
-    //quanto mais distante da previsao do modelo da resposta correta
-    //maior o erro (loss)
-    // exemplo classico: classificaçao de imagens , recomendaçao ,categorização de usuario 
-    //qualquer coisa em que a resposta certa é "apenas uma entre varias possiveis"
+    // loss: categoricalCrossentropy
+    // Ele compara o que o modelo "acha" (os scores de cada categoria)
+    // com a resposta certa
+    // a categoria premium será sempre [1, 0, 0]
+
+    // quanto mais distante da previsão do modelo da resposta correta
+    // maior o erro (loss)
+    // Exemplo classico: classificação de imagens, recomendação, categorização de
+    // usuário
+    // qualquer coisa em que a resposta certa é "apenas uma entre várias possíveis"
+
     model.compile({
-         optimizer: 'adam',
-          loss: 'categoricalCrossentropy',
-           metrics: ['accuracy']
-        })
+        optimizer: 'adam',
+        loss: 'categoricalCrossentropy',
+        metrics: ['accuracy']
+    })
 
+    // Treinamento do modelo
+    // verbose: desabilita o log interno (e usa só callback)
+    // epochs: quantidade de veses que vai rodar no dataset
+    // shuffle: embaralha os dados, para evitar viés
+    await model.fit(
+        inputXs,
+        outputYs,
+        {
+            verbose: 0,
+            epochs: 100,
+            shuffle: true,
+            callbacks: {
+                // onEpochEnd: (epoch, log) => console.log(
+                //     `Epoch: ${epoch}: loss = ${log.loss}`
+                // )
+            }
+        }
+    )
 
-        
+    return model
+}
+
+async function predict(model, pessoa) {
+    // transformar o array js para o tensor (tfjs)
+    const tfInput = tf.tensor2d(pessoa)
+
+    // Faz a predição (output será um vetor de 3 probabilidades)
+    const pred = model.predict(tfInput)
+    const predArray = await pred.array()
+    return predArray[0].map((prob, index) => ({ prob, index }))
 }
 // Exemplo de pessoas para treino (cada pessoa com idade, cor e localização)
 // const pessoas = [
@@ -79,5 +115,30 @@ const tensorLabels = [
 const inputXs = tf.tensor2d(tensorPessoasNormalizado)
 const outputYs = tf.tensor2d(tensorLabels)
 
+// quanto mais dado melhor!
+// assim o algoritmo consegue entender melhor os padrões complexos
+// dos dados
+const model = await trainModel(inputXs, outputYs)
 
-const models = trainModel(inputXs, outputYs)
+const pessoa = { nome: 'zé', idade: 28, cor: 'verde', localizacao: "Curitiba" }
+// Normalizando a idade da nova pessoa usando o mesmo padrão do treino
+// Exemplo: idade_min = 25, idade_max = 40, então (28 - 25) / (40 - 25 ) = 0.2
+
+const pessoaTensorNormalizado = [
+    [
+        0.2, // idade normalizada
+        1,    // cor azul
+        0,    // cor vermelho
+        0,    // cor verde
+        1,    // localização São Paulo
+        1,    // localização Rio
+        0     // localização Curitiba
+    ]
+]
+
+const predictions = await predict(model, pessoaTensorNormalizado)
+const results = predictions
+    .sort((a, b) => b.prob - a.prob)
+    .map(p => `${labelsNomes[p.index]} (${(p.prob * 100).toFixed(2)}%)`)
+    .join('\n')
+console.log(results)
