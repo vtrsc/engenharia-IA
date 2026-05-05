eb7572a1d8bf95be1cfa3bc3e0b13a6bca082384
vtrsc <vitorsc1998@gmail.com>
2026-05-05 10:23:35 -0300

    Como funcionam Sistemas de Recomendação

--- exemplo-01-ecommerce-recomendations/src/workers/modelTrainingWorker.js
+++ exemplo-01-ecommerce-recomendations/src/workers/modelTrainingWorker.js
@@ -1,41 +1,100 @@
 import 'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.22.0/dist/tf.min.js';
 import { workerEvents } from '../events/constants.js';
 
-console.log('Model training worker initialized');
-let _globalCtx = {};
+
+// 🔢 Normalize continuous values (price, age) to 0–1 range
+// Why? Keeps all features balanced so no one dominates training
+// Formula: (val - min) / (max - min)
+// Example: price=129.99, minPrice=39.99, maxPrice=199.99 → 0.56
+const normalize = (value, min, max) => (value - min) / ((max - min) || 1)
+
+function makeContext(products, users) {
+    const ages = users.map(u => u.age)
+    const prices = products.map(p => p.price)
+
+    const minAge = Math.min(...ages)
+    const maxAge = Math.max(...ages)
+
+    const minPrice = Math.min(...prices)
+    const maxPrice = Math.max(...prices)
+
+    const colors = [...new Set(products.map(p => p.color))]
+    const categories = [...new Set(products.map(p => p.category))]
+
+    const colorsIndex = Object.fromEntries(
+        colors.map((color, index) => {
+            return [color, index]
+        }))
+    const categoriesIndex = Object.fromEntries(
+        categories.map((category, index) => {
+            return [category, index]
+        }))
+
+    // Computar a média de idade dos comprados por produto
+    // (ajuda a personalizar)
+    const midAge = (minAge + maxAge) / 2
+    const ageSums = {}
+    const ageCounts = {}
+
+    users.forEach(user => {
+        user.purchases.forEach(p => {
+            ageSums[p.name] = (ageSums[p.name] || 0) + user.age
+            ageCounts[p.name] = (ageCounts[p.name] || 0) + 1
+        })
+    })
+
+    const productAvgAgeNorm = Object.fromEntries(
+        products.map(product => {
+            const avg = ageCounts[product.name] ?
+                ageSums[product.name] / ageCounts[product.name] :
+                midAge
+
+            return [product.name, normalize(avg, minAge, maxAge)]
+        })
+    )
+
+    return {
+        products,
+        users,
+        colorsIndex,
+        categoriesIndex,
+        productAvgAgeNorm,
+        minAge,
+        maxAge,
+        minPrice,
+        maxPrice,
+        numCategories: categories.length,
+        numColors: colors.length,
+        // price + age + colors + categories
+        dimentions: 2 + categories.length + colors.length
+    }
+}
 
 
 async function trainModel({ users }) {
-    console.log('Training model with users:', users)
+    console.log('Training model with users:', users);
+    postMessage({ type: workerEvents.progressUpdate, progress: { progress: 1 } });
+    const products = await (await fetch('/data/products.json')).json()
 
-    postMessage({ type: workerEvents.progressUpdate, progress: { progress: 50 } });
-    postMessage({
-        type: workerEvents.trainingLog,
-        epoch: 1,
-        loss: 1,
-        accuracy: 1
-    });
+    const context = makeContext(products, users)
+    
+    debugger
 
-    setTimeout(() => {
-        postMessage({ type: workerEvents.progressUpdate, progress: { progress: 100 } });
-        postMessage({ type: workerEvents.trainingComplete });
-    }, 1000);
-
-
+    postMessage({ type: workerEvents.progressUpdate, progress: { progress: 100 } });
+    postMessage({ type: workerEvents.trainingComplete });
 }
-function recommend(user, ctx) {
-    console.log('will recommend for user:', user)
+function recommend({ user }) {
+
     // postMessage({
     //     type: workerEvents.recommend,
     //     user,
-    //     recommendations: []
+    //     recommendations: sortedItems
     // });
+
 }
-
-
 const handlers = {
     [workerEvents.trainModel]: trainModel,
-    [workerEvents.recommend]: d => recommend(d.user, _globalCtx),
+    [workerEvents.recommend]: recommend,
 };
 
 self.onmessage = e => {
