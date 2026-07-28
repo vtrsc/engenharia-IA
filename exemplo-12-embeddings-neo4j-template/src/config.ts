90572d2425d8ed49dc8492107788348bd8aa60d0
vtrsc <vitorsc1998@gmail.com>
2026-07-27 23:46:43 -0300

    Projeto: Criando o Orimeiro RAG Com JavaScript e Neo4j

--- exemplo-12-embeddings-neo4j-template/src/config.ts
+++ exemplo-12-embeddings-neo4j-template/src/config.ts
@@ -1,11 +1,4 @@
 import type { DataType, PretrainedModelOptions } from "@huggingface/transformers";
-import { readFileSync } from 'node:fs'
-
-const promptsFolder = './prompts';
-const promptsFiles = {
-    answerPrompt: `${promptsFolder}/answerPrompt.json`,
-    template: `${promptsFolder}/template.txt`,
-};
 
 export interface TextSplitterConfig {
     chunkSize: number;
@@ -13,12 +6,6 @@ export interface TextSplitterConfig {
 }
 
 export const CONFIG = Object.freeze({
-    promptConfig: JSON.parse(readFileSync(promptsFiles.answerPrompt, 'utf-8')),
-    templateText: readFileSync(promptsFiles.template, 'utf-8'),
-    output: {
-        answersFolder: './respostas',
-        fileName: 'resposta',
-    },
     neo4j: {
         url: process.env.NEO4J_URI!,
         username: process.env.NEO4J_USER!,
