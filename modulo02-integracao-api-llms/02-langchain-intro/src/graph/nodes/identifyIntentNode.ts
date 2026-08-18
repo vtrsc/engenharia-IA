c51d4dbd2c826e12a4e038f613894b665eb3cce1
vtrsc <vitorsc1998@gmail.com>
2026-08-18 17:07:02 -0300

    add langgraph.json

--- modulo02-integracao-api-llms/02-langchain-intro/src/graph/nodes/identifyIntentNode.ts
+++ modulo02-integracao-api-llms/02-langchain-intro/src/graph/nodes/identifyIntentNode.ts
@@ -1,12 +1,12 @@
 import { type GraphState } from "./graph.ts";
 
 export function identifyIntent(state: GraphState): GraphState {
-    const input = state.messages.at(-1)?.text ?? ""
+    const input = state.messages.at(-1)?.content?.toString() ?? ""
     const inputLower = input.toLowerCase()
 
     let command: GraphState['command'] = 'unknown'
 
-    if(inputLower.includes('upper')) {
+    if (inputLower.includes('upper')) {
         command = 'uppercase'
     } else if (inputLower.includes('lower')) {
         command = 'lowercase'
@@ -17,5 +17,4 @@ export function identifyIntent(state: GraphState): GraphState {
         command,
         output: input
     }
-
 }
