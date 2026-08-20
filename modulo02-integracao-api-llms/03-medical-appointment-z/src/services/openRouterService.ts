5d505f1b7c3fe89153658bf5adc7b33a54a07792
vtrsc <vitorsc1998@gmail.com>
2026-08-20 16:39:52 -0300

    Langchain.js - A Introdução - Definindo Node de Fallback, Implementando Casos de Teste Restantes

---
+++ modulo02-integracao-api-llms/03-medical-appointment-z/src/services/openRouterService.ts
@@ -0,0 +1,73 @@
+import { ChatOpenAI } from "@langchain/openai";
+import { config, type ModelConfig } from "../config.ts";
+import { z } from "zod/v3";
+import {
+    createAgent,
+    HumanMessage,
+    providerStrategy,
+    SystemMessage,
+} from "langchain";
+
+export class OpenRouterService {
+    private config: ModelConfig;
+    private llmClient: ChatOpenAI;
+
+    constructor(configOverride?: ModelConfig) {
+        this.config = configOverride ?? config;
+
+        this.llmClient = new ChatOpenAI({
+            apiKey: this.config.apiKey,
+            modelName: this.config.models.at(0),
+            temperature: this.config.temperature,
+
+            configuration: {
+                baseURL: "https://openrouter.ai/api/v1",
+                defaultHeaders: {
+                    "HTTP-Referer": this.config.httpReferer,
+                    "X-Title": this.config.xTitle,
+                },
+            },
+
+            modelKwargs: {
+                models: this.config.models,
+                provider: this.config.provider,
+            },
+        });
+    }
+
+    async generateStructured<T>(
+        systemPrompt: string,
+        userPrompt: string,
+        schema: z.ZodSchema<T>
+    ) {
+        try {
+            const agent = createAgent({
+                model: this.llmClient,
+                tools: [],
+                responseFormat: providerStrategy(schema),
+            });
+
+            const messages = [
+                new SystemMessage(systemPrompt),
+                new HumanMessage(userPrompt),
+            ];
+
+            const data = await agent.invoke({ messages });
+
+            return {
+                success: true as const,
+                data: data.structuredResponse,
+            };
+        } catch (error) {
+            console.error("❌ Error OpenRouterService:", error);
+
+            return {
+                success: false as const,
+                error:
+                    error instanceof Error
+                        ? error.message
+                        : String(error),
+            };
+        }
+    }
+}
